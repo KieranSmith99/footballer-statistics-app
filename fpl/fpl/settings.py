d2c72310a1c77886338d28662e17315a56cf8386
Kieran Smith <kieransmith63@gmail.com>
2021-01-12 14:35:04 +0000

    Hid key
    Secret key now hidden

--- fpl/fpl/settings.py
+++ fpl/fpl/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+from secrets import SECRET_KEY_SAFE
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'bx9)3r)^(7r0f$ov#mmckljm7#mdz1s)mm1qb0q(#tx^40_qcw'
+SECRET_KEY = SECRET_KEY_SAFE
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
